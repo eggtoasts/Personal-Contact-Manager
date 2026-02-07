54b5a53593e7fccb2b6a1f118f8895670b63f48e
Harmony Hamilton <harmonyhamilton9@icloud.com>
2026-02-07 18:01:11 -0500

    Update deleteContact.php

--- API Files/deleteContact.php
+++ API Files/deleteContact.php
@@ -1,3 +1,32 @@
 <?php
 
+    //Delete contact from contact table
+
+    require_once 'dp.php';
+    $inData = getRequestInfo();
+
+    $userId = $inData["userId"];
+    $email = $inData["email"];
+
+    $stmt = $pdo->prepare("DELETE FROM contacts_tb WHERE user_id = ? AND email = ?")
+
+    if($stmt->execute([$userId, $email])) {
+        returnWithError("");
+    } else {
+        returnWithError("Delete Contact Failed");
+    }
+
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err) {
+        $retValue = json_encode(["error" => $err]);
+        sendResultInfoAsJson($retValue);
+    }
 ?>
