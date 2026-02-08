da5aca8e7edfff13a1ec93265338229d8ba086a6
aileenchy <seidlaileen@gmail.com>
2026-02-08 16:30:06 -0500

    Update searchContacts.php
    Worked on the search api

--- backend/searchContacts.php
+++ backend/searchContacts.php
@@ -1,69 +1,72 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
+    require_once 'db.php';
+    $inData = getRequestInfo();
 
-    //modify this to match the admin of the our server
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager_db");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("select Name from Colors where Name like ? and UserID=?");
-		$colorName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $colorName, $inData["userId"]);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["Name"] . '"';
-		}
-		
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
-		
-		$stmt->close();
-		$conn->close();
-	}
+    $contactCount = 0;
+    $contactResults = "";
 
-	function getRequestInfo()
+
+    try{
+
+
+        $stmt = $pdo->prepare("SELECT first_name, last_name, phone, email, created from contacts_tb WHERE (first_name like ? OR last_name like ?) AND user_id = ?");
+		$ContactName = "%" . $inData["search"] . "%";
+        $stmt->bind_param("ssi", $ContactName, $ContactName, $inData["userId"]);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        while($row = $result->fetch_assoc()) {
+
+            if( $contactCount > 0 ){
+                $contactResults .= ",";
+            }
+
+            $contactCount++;
+            $contactResults .= '{"firstName" : "' . $row["firstName"] . '"},
+                                {"lastName" : "' . $row["lastName"] . '"},
+                                {"email" : "' . $row["email"] . '"},
+                                {"phone" : "' . $row["phone"] . '"},
+                                {"created" : "' . $row["created"] . '"}
+                                ';
+        }
+
+        if($contactCount == 0){
+            returnWithError("No Contacts Found");
+        } else {
+            returnWithInfo( $contactResults );
+        }
+
+        $stmt->close();
+
+    } catch(PDOException $e){
+        returnWithError($e->getMessage());
+    }
+
+    function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+    function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
-	function returnWithError( $err )
+
+    function returnWithError( $err )
 	{
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-	function returnWithInfo( $searchResults )
+
+    function returnWithInfo( $searchResults )
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
+
+
 ?>
